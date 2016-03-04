4ad6b6e091b0281f9e39e9d1a06c61e503abb29c
Sam Fife <sam.o.fife@gmail.com>
2016-03-04 10:58:21 -0600

    Add documentation

--- NCAA_predictions.py
+++ NCAA_predictions.py
@@ -18,84 +18,75 @@ tour_slot = pd.read_csv("TourneySlots.csv")
 # print(teams.head())
 # print(tour_slot)
 
-print(tour_slot.loc[tour_slot["Season"] == 2015])
+# print(tour_slot.loc[tour_slot["Season"] == 2015])
 # print(tour_seed.loc[tour_seed["Season"] == 2015])
 
 def set_matchups(season):
-	"""Solve matchups for a given season"""
+	"""Compile list of matchups for a given season"""
 
-	slots = list(tour_slot["Slot"].loc[tour_slot["Season"] == season])
-	strongseeds = list(tour_slot["Strongseed"].loc[tour_slot["Season"] == season])
-	weakseeds = list(tour_slot["Weakseed"].loc[tour_slot["Season"] == season])
-	seeds = list(tour_seed["Seed"].loc[tour_seed["Season"] == season])
-	teams = list(tour_seed["Team"].loc[tour_seed["Season"] == season])
-	team_dict = {seeds[i]:teams[i] for i in range(0,len(seeds))}
-	print(seeds)
-	print(team_dict)
+	slots = list(tour_slot["Slot"].loc[tour_slot["Season"] == season]) # get list of slots, e.g. R1W1
+	strongseeds = list(tour_slot["Strongseed"].loc[tour_slot["Season"] == season]) # get list of strongseeds
+	weakseeds = list(tour_slot["Weakseed"].loc[tour_slot["Season"] == season]) # get list of weakseeds
+	seeds = list(tour_seed["Seed"].loc[tour_seed["Season"] == season]) # get list of seeds
+	teams = list(tour_seed["Team"].loc[tour_seed["Season"] == season]) # get list of teams ordered by seed
 
-	slot_dict = {slots[j]:[strongseeds[j], weakseeds[j]] for j in range(0, len(slots))} 
-	print(slot_dict)
+	team_dict = {seeds[i]:teams[i] for i in range(0,len(seeds))} # make dict of teams by seed (probably redundant)
+	# print(team_dict)
+	slot_dict = {slots[j]:[strongseeds[j], weakseeds[j]] for j in range(0, len(slots))} # make dict of slots, value = slots that comprise key slot
+	# print(slot_dict)
 
-	print(slots[0])
-
-	eligible = {}
-	count = 0
+	eligible = {} # initialize empty dict of teams by slot, {"slot":[[strongseed list],[weakseed list]]}
+	# count = 0 # used for testing
 	for s in slots:
-		eligible[s] = []
+		eligible[s] = [] # each slot has a list of 2 sets of teams that could face each other
 		for t in slot_dict[s]:
-			print("t = ",t)
-			if t in team_dict:
+			# print("t = ",t)
+			if t in team_dict: # if slot component is a team, create a list element of the team id
 				# print(1)
 				team_id = []
-				team_id.append(team_dict[t])
+				team_id.append(team_dict[t]) # could probably combine these 2 lines into team_id = list(team_dict[t])
 				# print("team_id = ",team_id)
-				eligible[s].append(team_id)
+				eligible[s].append(team_id) # append team_id to eligible teams for that slot
 				# print(2)
-			elif t in eligible:
-				print(eligible[t])
+			elif t in eligible: # if t is a slot itself
+				# print(eligible[t])
 				temp_list = []
-				for u in eligible[t]:
-					print("u = ",u)
-					if type(u) is list:
+				for u in eligible[t]: # for teams from prior round, add them all to a list of teams for 1/2 of this slots matchups
+					# print("u = ",u)
+					if type(u) is list: # to avoid nesting lists, 
 						temp_list += u
 					else:
-						temp_list.append(u)
-				print("u2 =",temp_list)
-				# eligible[s] += temp_list
-				eligible[s].append(temp_list)
+						temp_list.append(u) # should all be list now, legacy element, should never fire
+				# print("u2 =",temp_list)
+				eligible[s].append(temp_list) # add list of opponents to eligible slot for that round
 			else:
 				pass
-		count += 1
+		# count += 1 # for testing
 		# if count > 50:
 		# 	break
-		
-	print(eligible)
 
-	lengths = []
-	for key in eligible:
-		lengths.append(len(eligible[key]))
+	# lengths = []
+	# for key in eligible:
+	# 	lengths.append(len(eligible[key]))
 		# pass
+	# print(lengths, len(lengths)) # testing to make sure all eligible has length 2
 
-	print(lengths, len(lengths))
+	matchups = [] # initialize list of matchups
+	for key in eligible: # iterate slots
+		group_A = eligible[key][0] # take first list, could replace in list comp below
+		group_B = eligible[key][1] # take second list, could replace in list comp below
+		# list comp for all matchups for a given slot
+		temp_match = [str(season)+'_'+str(min(a,b))+'_'+str(max(a,b)) for a in group_A for b in group_B]
+		matchups += temp_match # append to matchups
 
-	matchups = []
-	for key in eligible:
-		group_A = eligible[key][0]
-		group_B = eligible[key][1]
-		for a in group_A:
-			# temp_match = []
-			for b in group_B:
-				
-				# print(temp_match)
-				matchups.append(str(season)+'_'+str(min(a,b))+'_'+str(max(a,b)))
-				# print(matchups)
-				# break
-			# break
-		# break
+	print(matchups, len(matchups)) # test for correct # of matchups
 
-	print(matchups, len(matchups))
-
-	return None
+	return matchups
 
 set_matchups(2015)
+
+
+
+
+
 
